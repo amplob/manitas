d9bfc19f152bfa5392330bc6b8d91645225e89bd
Marc <amplob@gmail.com>
2024-08-09 07:20:41 +0200

    comment one import

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-import { createClient } from '@supabase/supabase-js'
+//import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://uebruygyrwmsqtmskcdz.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
