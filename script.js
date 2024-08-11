fe9f030ae9f27d3ceda0348ec7ff0361b3ad8e1c
amplob <amplob@gmail.com>
2024-08-11 18:36:02 +0200

    added key for supabase and prepared the .env file in case its possible

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 //import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://uebruygyrwmsqtmskcdz.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+//const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlYnJ1eWd5cndtc3F0bXNrY2R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNzM2MzYsImV4cCI6MjAzODc0OTYzNn0.kCWtxwaAu7hMbHAy2Q5Dc2-RMWmhrk27a1__32I0y58
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
