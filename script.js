7f093edc8a832e70a77ff72aee1802967b48be32
amplob <amplob@gmail.com>
2024-08-11 18:44:57 +0200

    imported createClient

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://uebruygyrwmsqtmskcdz.supabase.co'
 //const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
